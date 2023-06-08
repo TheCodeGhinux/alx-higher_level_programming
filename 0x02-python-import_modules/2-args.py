13576ca3aa35314a5082f46462a7a2a1af41bf19
Ayomide <ilesanmibry@gmail.com>
2023-06-08 11:11:56 -0700

    commit

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+
+    a = sys.argv
+    print(a)
+    arg_len = len(a) - 1
+    if arg_len == 0:
+        print("0 arguments.")
+    elif arg_len == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(arg_len))
+    for b in range(arg_len):
+        print("{}: {}".format(b + 1, a[b + 1]))
