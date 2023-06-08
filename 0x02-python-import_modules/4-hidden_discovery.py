d80a020b8b3f59178232cac4fd4a99c0a492bf39
Ayomide <ilesanmibry@gmail.com>
2023-06-08 11:45:51 -0700

    commit

--- 0x02-python-import_modules/4-hidden_discovery.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-if __name__ == "__main__":
-    from hidden_4 import *
-
-    names = ()
-    for n in range(len(names)):
-        if names[n][:2] != "__":
-            print("{}".format(names[n]))
