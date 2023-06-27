2aa4003cd1f743ceccb9d6787e3e491a8db8b700
Ayomide <ilesanmibry@gmail.com>
2023-06-26 21:02:41 -0700

    commit

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -7,7 +7,7 @@ def magic_calculation(a, b):
                 raise Exception('Too far')
             else:
                 res += a ** b / x
-        except:
+        except Exception as e:
             res = b + a
             break
     return (res)
