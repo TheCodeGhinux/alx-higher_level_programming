126445199182744e6c8724c86cd13e3ce9b2bbe1
Ayomide <ilesanmibry@gmail.com>
2023-06-30 07:12:35 +0000

    commit

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"Square class"
+
+
 class Square:
+    "To represent a square"
     def __init__(self, size):
+        "Initialize a new square"
         self.__size = size
---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"Square class"
+
+
+class Square:
+    """Initialize a Square object with a given size.
+        
+        Args:
+            size (int): The size of the square (default: 0).
+    """
+    def __init__(self, size=0):
+        "Get the size of the square."
+        self.size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
