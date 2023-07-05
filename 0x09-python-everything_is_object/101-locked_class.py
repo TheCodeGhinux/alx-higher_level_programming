55d4e5e1162a0e4b89e1afbcec12577433b184b9
Ayomide <ilesanmibry@gmail.com>
2023-07-05 06:50:53 -0700

    commit

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 "Defines a class LockedClass"
 
+
 class LockedClass:
-    """To prevents the user from dynamically 
-      creating new instance attributes, except if the 
+    """To prevents the user from dynamically
+      creating new instance attributes, except if the
       new instance attribute is called first_name"""
 
-    
     __slots__ = ('first_name',)
 
     def __setattr__(self, name, value):
         if name != 'first_name':
-            raise AttributeError(f"Cannot create new instance attribute '{name}'")
+            raise AttributeError(f"Cannot create new instance attr '{name}'")
         self.__dict__[name] = value
