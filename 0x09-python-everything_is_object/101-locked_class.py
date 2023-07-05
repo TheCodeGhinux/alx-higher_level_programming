0b09d00b2bc6581b7f8764a839b1aeb9dc8f61e7
Ayomide <ilesanmibry@gmail.com>
2023-07-05 06:53:00 -0700

    commit

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -7,9 +7,4 @@ class LockedClass:
       creating new instance attributes, except if the
       new instance attribute is called first_name"""
 
-    __slots__ = ('first_name',)
-
-    def __setattr__(self, name, value):
-        if name != 'first_name':
-            raise AttributeError(f"Cannot create new instance attr '{name}'")
-        self.__dict__[name] = value
+    __slots__ = ['first_name']
