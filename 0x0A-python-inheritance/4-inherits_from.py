1e018aa20040a0090464eeb7ff0cc694e5a39570
Ayomide Ilesanmi <65258939+TheCodeGhinux@users.noreply.github.com>
2023-07-11 03:52:52 +0100

    Update 4-inherits_from.py

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -7,4 +7,5 @@ Define inherits_from function.
 def inherits_from(obj, a_class):
     """Return obj inherits from a_class.
     """
+    
     return(issubclass(type(obj), a_class) and not (type(obj) is a_class))
