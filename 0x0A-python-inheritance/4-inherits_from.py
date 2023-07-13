95165f32c9379b017828702884e82fac42b67ee2
Ayomide <ilesanmibry@gmail.com>
2023-07-12 19:03:35 -0700

    commit

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -8,4 +8,4 @@ def inherits_from(obj, a_class):
     """Return obj inherits from a_class.
     """
 
-    return(issubclass(type(obj), a_class) and not (type(obj) is a_class))
+    return (issubclass(type(obj), a_class) and not (type(obj) is a_class))
