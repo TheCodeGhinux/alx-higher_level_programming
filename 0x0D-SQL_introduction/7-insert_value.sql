2e359fed9c29236abcb7b0e36c1c633890a6a41f
Ayomide Ilesanmi <65258939+TheCodeGhinux@users.noreply.github.com>
2023-08-16 10:53:45 +0100

    Update 7-insert_value.sql

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,3 +1,3 @@
 -- insett these values into the tableid = 89,
 -- name = Holberton School
-INSERT INTO `first_table` (`id`, `name`) VALUES ("89", "Holberton School");
+INSERT INTO `first_table` (`id`, `name`) VALUES ("89", "Best School");
