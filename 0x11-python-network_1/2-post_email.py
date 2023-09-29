85e67d2f5ede9dd5e6266851c25febbd5afcfa77
Ayomide Ilesanmi <65258939+TheCodeGhinux@users.noreply.github.com>
2023-09-29 17:15:03 +0100

    Update 2-post_email.py

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -11,24 +11,24 @@ import sys
 
 """
     Check if both a URL and an
-    email are provided as arguments
+    email are inputed as args
 """
 
 if len(sys.argv) != 3:
     print("Usage: python script.py <URL> <email>")
     sys.exit(1)
 
-url = sys.argv[1]
-email = sys.argv[2]
+input_url = sys.argv[1]
+input_email = sys.argv[2]
 
 # Encode the email parameter
-data = urllib.parse.urlencode({'email': email}).encode('utf-8')
+data = urllib.parse.urlencode({'email': input_email}).encode('utf-8')
 
 try:
     # Send a POST request with the email parameter
-    with urllib.request.urlopen(url, data=data) as response:
+    with urllib.request.urlopen(input_url, data=data) as res:
         # Read and decode the response body
-        html = response.read().decode('utf-8')
-        print(html)
+        html_res = res.read().decode('utf-8')
+        print(html_res)
 except Exception as e:
     print(f"Error: {e}")
