c5938107c4ce4a7ed87f67fd31480e0c2b0cf7fd
Ayomide Ilesanmi <65258939+TheCodeGhinux@users.noreply.github.com>
2023-09-29 17:16:24 +0100

    Update 3-error_code.py

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -11,21 +11,22 @@ import urllib.error
 import sys
 
 """
-    To check if url is unputted
+    To check if url is inputted
 """
 
 if len(sys.argv) != 2:
     print("Usage: python script.py <URL>")
     sys.exit(1)
 
-url = sys.argv[1]
+input_url = sys.argv[1]
 
 try:
-    with urllib.request.urlopen(url) as response:
-        # Read and decode the response body
-        html = response.read().decode('utf-8')
-        print(html)
-except urllib.error.HTTPError as e:
-    print(f"Error code: {e.code}")
-except Exception as e:
-    print(f"Error: {e}")
+    with urllib.request.urlopen(input_url) as res:
+        """ Read the body
+        """
+        html_res = res.read().decode('utf-8')
+        print(html_res)
+except urllib.error.HTTPError as err:
+    print(f"Error code: {err.code}")
+except Exception as err:
+    print(f"Error: {err}")
