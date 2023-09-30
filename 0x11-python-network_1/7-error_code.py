bedc85cbf6a51f808e9b767fe3d96c811c57c7aa
Ayomide Ilesanmi <65258939+TheCodeGhinux@users.noreply.github.com>
2023-09-30 06:53:33 +0100

    Update 7-error_code.py

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -14,17 +14,12 @@ Arguments:
 import requests
 import sys
 
-# Get url from command-line arguments
 input_url = sys.argv[1]
 
 try:
-    # Send a GET request to the url
     res = requests.get(input_url)
-
-    # Display the res body
     print(res.text)
 
-    # Check the HTTP status code
     if res.status_code >= 400:
         print(f"Error code: {res.status_code}")
 
