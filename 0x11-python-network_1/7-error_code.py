642ee120739bc0897aeaf88afa45f904cdf872e4
Ayomide Ilesanmi <65258939+TheCodeGhinux@users.noreply.github.com>
2023-09-30 06:56:28 +0100

    Update 7-error_code.py

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,18 +1,13 @@
 #!/usr/bin/python3
-
-"""Python script that send a web req"""
+"""send a web req with error handling"""
 
 import requests
 import sys
 
-input_url = sys.argv[1]
 
-try:
-    res = requests.get(input_url)
-    print(res.text)
-
-    if res.status_code >= 400:
+if __name__ == '__main__':
+    res = requests.get(sys.argv[1])
+    if res.status_code > 400:
         print(f"Error code: {res.status_code}")
-
-except requests.exceptions.RequestException as err:
-    print(f"Error: {err}")
+    else:
+        print(response.text)
