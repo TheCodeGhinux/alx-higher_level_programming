a6df979675d8abacbfa4df68ee1f1ae529c4ef51
Ayomide Ilesanmi <65258939+TheCodeGhinux@users.noreply.github.com>
2023-09-30 07:27:03 +0100

    Update 7-error_code.py

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,14 +1,23 @@
 #!/usr/bin/python3
 """Python script that sends a web req with error handling"""
+
 import requests
 import sys
 
+"""Python script that sends a web req with error handling"""
 try:
+    """Python script that sends a web req with error handling"""
     res = requests.get(sys.argv[1])
+    """Python script that sends a web req with error handling"""
     if res.status_code > 400:
+        """Python script that sends a web req with error handling"""
         print(f"Error code: {res.status_code}")
+    """Python script that sends a web req with error handling"""
     else:
+        """Python script that sends a web req with error handling"""
         print(res.text)
 
+"""Python script that sends a web req with error handling"""
 except requests.exceptions.RequestException as err:
+    """Python script that sends a web req with error handling"""
     print(f"Error: {err}")
