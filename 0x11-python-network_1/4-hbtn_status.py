a5800a72607c771a59df1c3f15119064ac14e1fa
Ayomide Ilesanmi <65258939+TheCodeGhinux@users.noreply.github.com>
2023-09-30 06:44:24 +0100

    Update 4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -13,6 +13,8 @@ try:
     res.raise_for_status()
 
     # Display the response body with tabulation
-    print(f"Body response:\n\t- type: {type(res.text)}\n\t- content: {res.text}")
+    print("Body response:")
+    print("\t- type: {}".format(type(res.text)))
+    print("\t- content: {}".format(res.text))
 except requests.exceptions.RequestException as err:
     print(f"Error: {err}")
