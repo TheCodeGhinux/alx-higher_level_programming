5e8e8cef8584b9422eb6a5a211171f7daeeac5dd
Ayomide Ilesanmi <65258939+TheCodeGhinux@users.noreply.github.com>
2023-09-30 07:50:36 +0100

    Update 8-json_api.py

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -2,37 +2,32 @@
 """
 Python script that takes in a letter and sends a POST request
 to http://0.0.0.0:5000/search_user with the letter as a parameter.
-
-Usage:
-    python script.py <letter>
-
-Arguments:
-    <letter>: The letter to search for in the API.
-
-Example:
-    python script.py a
 """
 
 import requests
 import sys
 
-try:
-    # Input URL to send the POST request
+if __name__ == '__main__':
+    # Define the URL for the POST request
     url = "http://0.0.0.0:5000/search_user"
-    
-    """Extract the letter from command-line arguments,
-    or set it to an empty string if not provided"""
+
+    # Create a dictionary with the letter parameter
     if len(sys.argv) < 2:
         search = {'q': '""'}
     else:
         search = {'q': sys.argv[1]}
-    
+
+    # Send the POST request
     req = requests.post(url, search)
-    search = req.json()
-except ValueError:
-    print("Not a valid JSON")
-else:
-    if isinstance(search, list) and len(search) < 1:
-        print('No result')
+
+    try:
+        # Parse the JSON response
+        res = req.json()
+    except ValueError:
+        print("Not a valid JSON")
     else:
-        print(f"[{search[0]['id']}] {search[0]['name']}")
+        if isinstance(res, list) and len(res) < 1:
+            print('No result')
+        else:
+            # Access the 'id' and 'name' from the JSON response
+            print('[{}] {}'.format(res[0]['id'], res[0]['name']))
