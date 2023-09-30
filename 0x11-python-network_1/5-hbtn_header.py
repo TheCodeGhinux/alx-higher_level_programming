dbdd9281f76aef066a8d9c7e5dd9a70bd69c1aa1
Ayomide Ilesanmi <65258939+TheCodeGhinux@users.noreply.github.com>
2023-09-30 05:39:30 +0100

    Update 5-hbtn_header.py

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,23 +1,31 @@
 #!/usr/bin/python3
+
+"""
+    Python script that takes in a URL,
+    sends a request to the URL and displays
+    the value of the variable X-Request-Id
+    in the response header
+"""
+
 import requests
 import sys
 
-# Check if a URL is provided as an argument
+# Check if a url is provided as an argument
 if len(sys.argv) != 2:
-    print("Usage: python script.py <URL>")
+    print("Usage: python script.py <url>")
     sys.exit(1)
 
-url = sys.argv[1]
+input_url = sys.argv[1]
 
 try:
-    response = requests.get(url)
+    res = requests.get(input_url)
 
-    # Get the value of the X-Request-Id header
-    x_request_id = response.headers.get('X-Request-Id')
+    # Get value of the X-Request-Id header
+    x_request_id = res.headers.get('X-Request-Id')
 
     if x_request_id is not None:
         print(x_request_id)
     else:
         print("X-Request-Id header not found in the response.")
-except requests.exceptions.RequestException as e:
-    print(f"Error: {e}")
+except requests.exceptions.RequestException as err:
+    print(f"Error: {err}")
