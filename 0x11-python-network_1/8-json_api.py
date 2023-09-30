12a61bcfee09b4f6cb78d45e374859cf2c199b35
Ayomide Ilesanmi <65258939+TheCodeGhinux@users.noreply.github.com>
2023-09-30 05:29:58 +0100

    Update 8-json_api.py

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -2,42 +2,42 @@
 import requests
 import sys
 
-# Extract the letter from command-line arguments, or set it to an empty string if not provided
+""" Extract the letter from command-line arguments,
+or set it to an empty string if not provided"""
 if len(sys.argv) > 1:
-    q = sys.argv[1]
+    letter = sys.argv[1]
 else:
-    q = ""
+    letter = ""
 
 # Create a dictionary with the letter parameter
-data = {"q": q}
+data = {"q": letter}
 
-# Define the URL to send the POST request to
-url = "http://0.0.0.0:5000/search_user"
+# input url to send the POST request
+input_url = "http://0.0.0.0:5000/search_user"
 
 try:
     # Send a POST request with the letter parameter
-    response = requests.post(url, data=data)
+    res = requests.post(input_url, data=data)
 
     # Check if the response contains valid JSON and is not empty
-    if response.headers.get("content-type") == "application/json" and response.text:
-        # Parse the JSON response
-        response_json = response.json()
+    if res.headers.get("content-type") == "application/json" and res.text:
+        res_json = res.json()
 
-        # Check if the JSON contains 'id' and 'name' keys
-        if "id" in response_json and "name" in response_json:
-            user_id = response_json["id"]
-            user_name = response_json["name"]
+        # Check for the JSON 'id' and 'name' keys
+        if "id" in res_json and "name" in res_json:
+            user_id = res_json["id"]
+            user_name = res_json["name"]
             print(f"[{user_id}] {user_name}")
         else:
             print("Not a valid JSON")
 
-    # Handle the case where the JSON is empty
-    elif not response.text:
+    # Handle empty case
+    elif not res.text:
         print("No result")
 
-    # Handle the case where the response is not valid JSON
+    # Handle error if response is not available
     else:
         print("Not a valid JSON")
 
-except requests.exceptions.RequestException as e:
-    print(f"Error: {e}")
+except requests.exceptions.RequestException as err:
+    print(f"Error: {err}")
