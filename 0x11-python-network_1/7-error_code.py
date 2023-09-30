3a9c52f376862755bc429cfbbc2f5a90205ec2b8
Ayomide Ilesanmi <65258939+TheCodeGhinux@users.noreply.github.com>
2023-09-30 07:31:15 +0100

    Update 7-error_code.py

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,19 +1,15 @@
 #!/usr/bin/python3
 """
-    Python script that takes in a URL,
-    sends a request to the URL and
-    displays the body of the response.
+    Python script that takes in a URL
 """
 
 import requests
 import sys
 
-# Get URL from command-line arguments
-input_url = sys.argv[1]
 
 if __name__ == '__main__':
-    res = requests.get(input_url)
-    if res.status_code > 400:
-        print(f"Error code: {res.status_code}")
+    response = requests.get(sys.argv[1])
+    if response.status_code > 400:
+        print("Error code: {}".format(response.status_code))
     else:
-        print(res.text)
+        print(response.text)
