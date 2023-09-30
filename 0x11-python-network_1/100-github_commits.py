baed32f34630c88adbc9928cc1e08c5e8c37a511
Ayomide Ilesanmi <65258939+TheCodeGhinux@users.noreply.github.com>
2023-09-30 05:20:50 +0100

    Update 100-github_commits.py

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,35 +1,44 @@
 #!/usr/bin/python3
+
+"""
+    Python script that takes 2 arguments in order to
+    solve an interview challenge
+"""
+
 import requests
 import sys
 
-# Extract the repository name and owner name from command-line arguments
-repository_name = sys.argv[1]
+"""
+    Extract the repo name and owner 
+    name from command-line arguments
+"""
+repo_name = sys.argv[1]
 owner_name = sys.argv[2]
 
 # Define the GitHub API URL for listing commits
-url = f"https://api.github.com/repos/{owner_name}/{repository_name}/commits"
+input_url = f"https://api.github.com/repos/{owner_name}/{repo_name}/commits"
 
 # Set the parameters to get the most recent 10 commits
 params = {"per_page": 10, "page": 1}
 
 try:
-    # Send a GET request to the GitHub API
-    response = requests.get(url, params=params)
+    # Send a GET request to GitHub
+    res = requests.get(input_url, params=params)
 
-    # Check if the request was successful (status code 200)
-    if response.status_code == 200:
-        # Parse the response JSON
-        commits = response.json()
+    """ Check if request was successful
+    and return status code 200 """
+    if res.status_code == 200:
+        commits = res.json()
 
-        # Print the list of commits with their SHA and author names
+        # Print list of commits with their SHA and author names
         for commit in commits:
             sha = commit.get("sha")
             author_name = commit["commit"]["author"]["name"]
             print(f"{sha}: {author_name}")
 
-    # Handle the case where the request fails
+    # Handle error for failure
     else:
-        print(f"Error: Received status code {response.status_code}")
+        print(f"Error: Received status code {res.status_code}")
 
-except requests.exceptions.RequestException as e:
-    print(f"Error: {e}")
+except requests.exceptions.RequestException as err:
+    print(f"Error: {err}")
