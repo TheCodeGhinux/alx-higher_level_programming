23cd72287f618cacf421fb1f87daf3de09ea537c
Ayomide Ilesanmi <65258939+TheCodeGhinux@users.noreply.github.com>
2023-09-30 07:30:07 +0100

    Update 7-error_code.py

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,23 +1,19 @@
 #!/usr/bin/python3
-"""Python script that sends a web req with error handling"""
+"""
+    Python script that takes in a URL,
+    sends a request to the URL and
+    displays the body of the response.
+"""
 
 import requests
 import sys
 
-"""Python script that sends a web req with error handling"""
-try:
-    """Python script that sends a web req with error handling"""
-    res = requests.get(sys.argv[1])
-    """Python script that sends a web req with error handling"""
-    if res.status_code > 400:
-        """Python script that sends a web req with error handling"""
-        print(f"Error code: {res.status_code}")
-    """Python script that sends a web req with error handling"""
-    else:
-        """Python script that sends a web req with error handling"""
-        print(res.text)
+# Get URL from command-line arguments
+input_url = sys.argv[1]
 
-"""Python script that sends a web req with error handling"""
-except requests.exceptions.RequestException as err:
-    """Python script that sends a web req with error handling"""
-    print(f"Error: {err}")
+if __name__ == '__main__':
+    res = requests.get(input_url)
+    if res.status_code > 400:
+        print(f"Error code: {res.status_code}")
+    else:
+        print(res.text)
