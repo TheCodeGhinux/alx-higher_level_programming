0c362d5263885c872eb164991a0e6472319e5b67
Ayomide Ilesanmi <65258939+TheCodeGhinux@users.noreply.github.com>
2023-09-30 07:46:00 +0100

    Update 8-json_api.py

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,26 +1,25 @@
 #!/usr/bin/python3
 
 """
-    Python script that takes in a letter and sends a POST request
-    to http://0.0.0.0:5000/search_user with the letter as a parameter.
+Python script that takes in a letter and sends a POST request
+to http://0.0.0.0:5000/search_user with the letter as a parameter.
 """
 
 import requests
 import sys
 
-# input url to send the POST request
-url = "http://0.0.0.0:5000/search_user"
-
-""" Extract the letter from command-line arguments,
-or set it to an empty string if not provided"""
-if len(sys.argv) < 2:
-    search = {'q': '""'}
-else:
-    search = {'q': sys.argv[1]}
-
-req = requests.post(url, search)
-
 try:
+    # Input URL to send the POST request
+    url = "http://0.0.0.0:5000/search_user"
+    
+    # Extract the letter from command-line arguments,
+    # or set it to an empty string if not provided
+    if len(sys.argv) < 2:
+        search = {'q': '""'}
+    else:
+        search = {'q': sys.argv[1]}
+    
+    req = requests.post(url, search)
     search = req.json()
 except ValueError:
     print("Not a valid JSON")
@@ -28,8 +27,4 @@ else:
     if isinstance(search, list) and len(search) < 1:
         print('No result')
     else:
-        print(f"[{user['id']}] {user['name']}")
-
-
-
-
+        print(f"[{search[0]['id']}] {search[0]['name']}")
