3f5c5e014aa71df73d3997a45ca39fc6c6b6fce9
Ayomide <ilesanmibry@gmail.com>
2023-09-29 23:12:43 -0700

    commit

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -7,6 +7,6 @@ import sys
 try:
     res = requests.get(sys.argv[1])
     if res.status_code > 400:
-    print(f"Error code: {res.status_code}")
+        print(f"Error code: {res.status_code}")
     else:
         print(response.text)
