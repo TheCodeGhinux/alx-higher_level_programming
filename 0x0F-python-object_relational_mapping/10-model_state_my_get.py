8e9b09a17f19055642ffaa8accb321014eb2bd18
Ayomide <ilesanmibry@gmail.com>
2023-11-19 10:04:44 -0800

    comit 5

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+"""
+Script that prints the State object with the
+name passed as argument from the database hbtn_0e_6_usa
+
+args: <mysql username> /
+      <mysql password> /
+      <database name>
+      <state name to search>
+"""
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import State, Base
+
+if __name__ == "__main__":
+    username, password, database, state_name = sys.argv[1:5]
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                          .format(username, password, database),
+                          pool_pre_ping=True)
+    """Bind the engine to the Base class"""
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state = session.query(State).filter(State.name == state_name).first()
+    if state:
+        print(state.id)
+    else:
+        print("Not found")
