ed34abfa8cccd7321ca14b2b2252cd3aa7560510
Ayomide Ilesanmi <65258939+TheCodeGhinux@users.noreply.github.com>
2023-11-19 18:11:32 +0000

    Update 9-model_state_filter_a.py

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -16,9 +16,9 @@ from model_state import State, Base
 
 if __name__ == "__main__":
     username, password, database = sys.argv[1:4]
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                          .format(username, password, database),
-                          pool_pre_ping=True)
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
+                           .format(username, password, database),
+                           pool_pre_ping=True)
     """Bind the engine to the Base class"""
     Base.metadata.create_all(engine)
 
@@ -27,5 +27,5 @@ if __name__ == "__main__":
 
     states = session.query(State).order_by(State.id)
     for state in states:
-      if "a" in state.name:
-          print("{}: {}".format(state.id, state.name))
+        if "a" in state.name:
+            print("{}: {}".format(state.id, state.name))
