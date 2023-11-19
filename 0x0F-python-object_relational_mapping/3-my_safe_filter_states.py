92a7cd24c3b5cb65077b10e69807bdc7cbce8fd6
Ayomide Ilesanmi <65258939+TheCodeGhinux@users.noreply.github.com>
2023-11-19 11:33:04 +0000

    Update 3-my_safe_filter_states.py

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -20,27 +20,22 @@ if __name__ == "__main__":
     args: Username, password, database, state_name
     """
 
-    """ Check if the correct number of arguments is provided"""
-    if len(sys.argv) != 5:
-        print("Usage: {} <username> <password> <database> <state_name>".format(sys.argv[0]))
-        sys.exit(1)
-
     username, password, database, state_name = sys.argv[1:5]
     """ Connect to the db server"""
-        db = MySQLdb.connect(
-            host="localhost",
-            port=3306,
-            user=username,
-            passwd=password,
-            db=database,
-            charset="utf8"
-        )
-        cursor = db.cursor()
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database,
+        charset="utf8"
+    )
+    cursor = db.cursor()
 
-        """ Use parameterized query to avoid SQL injection"""
-        query = "SELECT * FROM `states` WHERE `name` = %s ORDER BY `id`"
-        cursor.execute(query, (state_name,))
+    """ Use parameterized query to avoid SQL injection"""
+    query = "SELECT * FROM `states` WHERE `name` = %s ORDER BY `id`"
+    cursor.execute(query, (state_name,))
 
-        """Print results"""
-        for state in cursor.fetchall():
-            print(state)
+    """Print results"""
+    for state in cursor.fetchall():
+        print(state)
