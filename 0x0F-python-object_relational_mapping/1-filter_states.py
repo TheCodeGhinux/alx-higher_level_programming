3d5e611beba5f2bb29688397b3956a0eb3cfa93b
Ayomide <ilesanmibry@gmail.com>
2023-11-19 02:53:26 -0800

    commit 2

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+    Script to List all states with a name starting
+    with N from the database hbtn_0e_0_usa.
+
+    args: <mysql username>
+        <mysql password>
+        <database name>
+"""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    """
+    Check for correct arguments
+    args: Username, password, database
+    """
+
+    username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
+    
+    """ Connect the db server"""
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database,
+        charset="utf8"
+    )
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM `states` ORDER BY `id`")
+    
+    """Print the state"""
+    [print(state) for state in cursor.fetchall() if state[1][0] == "N"]
+
