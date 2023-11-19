7ab471094bbcdc3da721b27a0070c919ec84131f
Ayomide Ilesanmi <65258939+TheCodeGhinux@users.noreply.github.com>
2023-11-19 11:28:40 +0000

    Update 3-my_safe_filter_states.py

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """
-    Script that takes in arguments and displays
-    all values in the states table of hbtn_0e_0_usa where
-    name matches the argument. But this time, write one
-    that is safe from MySQL injections!
+Script that takes in arguments and displays
+all values in the states table of hbtn_0e_0_usa where
+name matches the argument. This version is safe from
+MySQL injections.
 
-    args: <mysql username>
-        <mysql password>
-        <database name>
+args: <mysql username>
+    <mysql password>
+    <database name>
 """
 
 import sys
@@ -19,20 +19,29 @@ if __name__ == "__main__":
     args: Username, password, database
     """
 
+    # Check if the correct number of arguments is provided
+    if len(sys.argv) != 4:
+        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
+        sys.exit(1)
+
     username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
 
-    """ Connect the db server"""
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=username,
-        passwd=password,
-        db=database,
-        charset="utf8"
-    )
-    cursor = db.cursor()
-    cursor.execute("SELECT * FROM `states`")
+    try:
+        """ Connect to the db server"""
+        db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=username,
+            passwd=password,
+            db=database,
+            charset="utf8"
+        )
+        cursor = db.cursor()
 
-    """Print results"""
-    for state in cursor.fetchall():
-        print(state)
+        # Use parameterized query to avoid SQL injection
+        query = "SELECT * FROM `states`"
+        cursor.execute(query)
+
+        """Print results"""
+        for state in cursor.fetchall():
+            print(state)
