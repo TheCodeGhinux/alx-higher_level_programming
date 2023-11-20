f960de1b8d2ca991a340a1f94eb08222dea9d7d9
Ayomide Ilesanmi <65258939+TheCodeGhinux@users.noreply.github.com>
2023-11-20 07:36:34 +0000

    Update 13-model_state_delete_a.py

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-
 """
-Script  that deletes all State objects with
-a name containing the letter a
+Script that changes the name of a State object
 from the database hbtn_0e_6_usa
 
 args: <mysql username> /
@@ -26,8 +24,13 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    states = session.query(State)
-    for state in states:
-        if "a" in state.name:
-            session.delete(state)
-    session.commit()
+    # Fetch the State with id = 2
+    update_state = session.query(State).filter(State.id == 2).first()
+
+    # Check if the state exists
+    if state_to_update:
+        # Change the name to 'New Mexico'
+        update_state.name = 'New Mexico'
+        session.commit()
+    else:
+        print("State with id = 2 not found.")
