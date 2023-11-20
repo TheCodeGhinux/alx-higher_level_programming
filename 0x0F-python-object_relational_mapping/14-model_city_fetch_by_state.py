de47f0bade2e8e41979898ee1e311eeabe972165
Ayomide Ilesanmi <65258939+TheCodeGhinux@users.noreply.github.com>
2023-11-20 07:18:57 +0000

    Update 14-model_city_fetch_by_state.py

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -18,20 +18,28 @@ from model_city import City
 if __name__ == "__main__":
     """
     Check for correct arguments
-    args: Username, password, database, state_name
+    args: Username, password, database
     """
+    # Extracting MySQL credentials from command line arguments
     username, password, database = sys.argv[1:4]
+
+    # Creating the SQLAlchemy engine with pool_pre_ping
     engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
                            .format(username, password, database),
                            pool_pre_ping=True)
-    """Bind the engine to the Base class"""
+
+    # Binding the engine to the Base class to create tables
     Base.metadata.create_all(engine)
 
+    # Creating a session
     Session = sessionmaker(bind=engine)
     session = Session()
 
+    # Querying City and State objects, filtering by state_id, and ordering by City id
     data = session.query(City, State) \
                   .filter(City.state_id == State.id) \
                   .order_by(City.id)
+
+    # Printing the results
     for city, state in data:
-          print("{}: ({}) {}".format(state.name, city.id, city.name))
+        print("{}: ({}) {}".format(state.name, city.id, city.name))
