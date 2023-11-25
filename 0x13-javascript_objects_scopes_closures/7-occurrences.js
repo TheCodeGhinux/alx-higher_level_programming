7a05620d416802f0d53b7b590fd9af706486bc79
Ayomide <ilesanmibry@gmail.com>
2023-11-25 17:19:59 +0000

    commit 6

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-function nbOccurences (list, searchElement) {
+exports.nbOccurences = function (list, searchElement) {
   return list.reduce(
     (count, current) => (current === searchElement ? count + 1 : count),
     0
-  );
+  )
 }
-exports.modules = nbOccurences;
+
