16c03424fdca9c3e1d31ec39817fc063da03067f
Ayomide <ilesanmibry@gmail.com>
2024-01-13 21:00:38 +0000

    commit 1

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const request = require('request')
+if (process.argv.length !== 4) {
+  console.error('Usage: node 5-request_store.js <url> <filepath>')
+  process.exit(1)
+}
+
+apiUrl = process.argv[2]
+filePath = process.argv[3]
+request(apiUrl).pipe(fs.createWriteStream(filePath))
